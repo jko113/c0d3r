65e674b03d9d00bc66989872beb7adb67ee2d70f
Josh Brown <jbrown016@gmail.com>
2018-06-27 21:44:01 -0400

    formatted layout.hbs after ~30 minutes trying to figure out why render hated me. thats commit worthy, right? taking my dog for a walk and wondering who is leading who

--- index.js
+++ index.js
@@ -21,10 +21,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // need to handle 
 app.get('/', (req, res) => {
-    var raw = (req.session.passport.user._raw)
-    raw = (JSON.parse(raw))
-    res.send(raw)
-    // res.send(`WELCOME ${req.session.passport.user.username}!`)
+    // var raw = (req.session.passport.user._raw)
+    // raw = (JSON.parse(raw))
+    // res.send(raw)
+    if (req.session.passport){
+        res.send(`WELCOME ${req.session.passport.user.username}!`)
+    } else {
+        res.send('Welcome!')
+    }
 });
 
 
@@ -32,27 +36,30 @@ app.get('/', (req, res) => {
 // redirected here from login using the functions in auth
 // the stuff after the else will be handled through res.render and allow for user to input in form format
 // will also make the remake the function after .then as a named function passed in
-app.get('/newprofile', (req, res) => {
+app.get('/newprofile', ensureAuthenticated, (req, res) => {
     var userSession = req.session.passport.user
-    var rawParsed = JSON.parse(userSession.raw)
+    // console.log(userSession._raw);
+    // console.log(rawParsed);
     db.getUserByGithubId(userSession.id)
-        .then((data) => {
-            if(data){
-                res.redirect('/')
-            } else {
-                var locArr = rawParse.location.split(',');
+    .then((data) => {
+        if(data){
+            console.log('data exists');
+            res.redirect('/')
+        } else {
+            console.log('data doesnt exist');
+            // res.send(userSession)
+            var rawParsed = JSON.parse(userSession._raw)
+                var locArr = rawParsed.location.split(',');
                 var city = locArr[0];
                 var state = locArr[1];
-                res.render('homepage',{
+                res.render('makeprofile', {
                    alias: userSession.username,
                    gitHubId: userSession.id,
                    username: userSession.username,
                    gitURL: userSession.profileUrl,
                    city: city,
                    state: state
-                }) 
-                db.addUser(userSession.username, userSession.id, userSession.username, userSession.displayName, userSession.displayName, userSession.profileUrl, null, null, null, null, new Date(), null, null, null, null)
-                res.send(userSession)    
+                });   
             }
     });
     // console.log(req.session.passport.user.username);
