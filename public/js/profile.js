3d95a7c63f383eb4855202a7df1b2c00e041f6df
Lisa Dean <lisa@lisadean.net>
2018-06-28 23:27:44 -0400

    Basic functionality of profile.hbs

---
+++ public/js/profile.js
@@ -0,0 +1,35 @@
+function toggleHidden(dataLabel) {
+  let elements = document.querySelectorAll(dataLabel);
+  elements.forEach(function(element) {
+    element.classList.toggle('is-hidden');
+  });
+}
+
+function addEditButtonListener() {
+  let button = document.querySelector('[data-edit-button]');
+  if(button){
+    button.addEventListener('click', function(event) {
+      toggleHidden('[data-edit-button]');
+      toggleHidden('[data-save-button]');
+      toggleHidden('[data-cancel-button]');
+      toggleHidden('[data-display]');
+    });
+  }
+}
+
+function addCancelListener() {
+  let button = document.querySelector('[data-cancel-button]');
+  if(button){
+    button.addEventListener('click', function(event) {
+      location.reload();
+      // TODO Replace reload with ajax request?
+    });
+  }
+}
+
+function main() {
+  addEditButtonListener();
+  addCancelListener();
+}
+
+main();
