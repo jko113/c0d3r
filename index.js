1e8d98dfe923c7bb2964b7c4350121080e44a8b1
Joshua Owens <jko113@gmail.com>
2018-07-01 14:07:32 -0400

    redirecting non-existent users from /search to /newprofile

--- index.js
+++ index.js
@@ -95,10 +95,10 @@ app.get('/newprofile', ensureAuthenticated, (req, res) => {
 app.post('/newprofile', (req, res) => {
     // let githubid = Number(req.body.githubid);
     let newBody = generateConvertedObject(req.body);
-    console.log(newBody);
+    // console.log(newBody);
     let zip = Number(req.body.zip_code);
     let userSession = req.session.passport.user;
-    console.log(userSession)
+    // console.log(userSession)
     let quotes = newBody.single_quotes_preference;
     let tabs = req.body.tabs_preference;
     let lines = req.body.same_line_curlies_preference;
@@ -137,16 +137,37 @@ app.post('/newprofile', (req, res) => {
             .catch(console.log)
         }
     })
-    
-    // dunno why this is here or if it is needed !!!!!!!!!!!!!
-// can revisit and reassess later as needed !!!!!!!!!!!!!!
-// app.get('/setup', ensureAuthenticated, (req, res) => {
-
-//     res.send(req.session.passport.user)
-// });
 
 app.get('/search', (req, res) => {
-    res.render('search')
+    // res.render('search')
+
+    const userData = req.session.passport.user;
+    const github_id = userData.id;
+
+    // check if user exists in database
+    db.checkUserExistence(github_id)
+        .then((data) => {
+
+            if (data && data.length) {
+
+                const isRegistered = data[0].user_exists;
+    
+                // render new messages page if user exists
+                if (isRegistered) {
+                    const internalId = data[0].user_id;
+                    res.render('search', {
+                        user_id: internalId
+                    })
+
+                // otherwise, redirect to root
+                } else {
+                    res.redirect('/');
+                }
+            } else {
+                res.redirect('/');
+            }
+        })
+        .catch(console.error);
 });
 
 app.post('/search', (req, res) => {
@@ -237,7 +258,7 @@ app.post('/home', (req, res) => {
             } else {
                 res.redirect('/login');
             }
-})
+        })
 })
 
 app.get('/messages', ensureAuthenticated, (req, res) => {
