46cf5490264a53682c2d960a3d61d1bdcc25306e
Josh Brown <jbrown016@gmail.com>
2018-07-02 12:38:46 -0400

    adjusted home.js to load users  in the right order

--- public/js/home.js
+++ public/js/home.js
@@ -6,12 +6,13 @@ function addLoadToggle(){
     if(load){
         let hiddenCards = document.querySelectorAll('.hide');
         let hiddenArr = Array.from(hiddenCards);
+        console.log(hiddenArr);
         load.addEventListener('click', function(event) {
             let count = 0;
             for (i=0; i<4; i++){
                 if (hiddenArr[0]){
                     hiddenArr[0].classList.remove('hide');
-                    hiddenArr = hiddenArr.slice(1, hiddenArr.length - 1)
+                    hiddenArr = hiddenArr.slice(1, hiddenArr.length)
                 } else {
                     load.classList.add('hide');
                 }
@@ -22,7 +23,7 @@ function addLoadToggle(){
 function hideAllCards(){
     let profileCards = document.body.querySelectorAll('.profile-container');
     let cardsArr = Array.from(profileCards);
-    let cutCardsArr = cardsArr.slice(4, cardsArr.length - 1);
+    let cutCardsArr = cardsArr.slice(5, cardsArr.length);
     console.log(cutCardsArr);
     for (i=0; i<cutCardsArr.length; i++){
         cutCardsArr[i].classList.add('hide');
