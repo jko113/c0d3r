fffcbdb32d1cd7a1925e649a295bc736cda04227
Josh Brown <jbrown016@gmail.com>
2018-07-03 15:24:47 -0400

    search resulsts vertical infinite scrolling feature enabled

--- public/js/home.js
+++ public/js/home.js
@@ -7,27 +7,34 @@ function hideAllCards(){
     if(cardsArr.length < 5 || !cardsArr){
         let load = document.body.querySelector('.load');
         load.classList.add('hide');
-    }
+    };
     let cutCardsArr = cardsArr.slice(5, cardsArr.length);
-    //console.log(cutCardsArr);
     for (i=0; i<cutCardsArr.length; i++){
         cutCardsArr[i].classList.add('hide');
-    }
-}
+    };
+};
 function scrollPage(){
+    $(window).scroll(function() {
+        setTimeout(scrollTimeout, 800);
+    });
+};
+
+function scrollTimeout() {
     let hiddenCards = document.querySelectorAll('.hide');
     let hiddenArr = Array.from(hiddenCards);
-    $(window).scroll(setTimeout(function() {
-        if($(window).scrollTop() + $(window).height() == $(document).height()) {
-            for (i=0; i<5; i++){
-                if (hiddenArr[0]){
-                    hiddenArr[0].classList.remove('hide');
-                    hiddenArr = hiddenArr.slice(1, hiddenArr.length)
-                }
-            }
-        }
-    }, 400));
-}
+    if($(window).scrollTop() + $(window).height() == $(document).height()) {
+        for (i=0; i<5; i++){
+            if (hiddenArr[0]){
+                hiddenArr[0].classList.remove('hide');
+                hiddenArr = hiddenArr.slice(1, hiddenArr.length);
+            } else {
+                let refreshButton = document.querySelector('.refresh');
+                refreshButton.classList.remove('hide');
+            };
+        };
+    };
+};
+
 
 hideAllCards();
 // scrollPage();
